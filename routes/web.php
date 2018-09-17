9401afdb57d8c9d82e5943452e3710a2a88b15aa
Steven Tune <Steven@Marks-MacBook-Pro-3.local>
2018-09-17 15:06:30 -0700

    adding auth routes

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,26 @@ Route::get('/sortCategories/{param}/{dir}', 'CategoryController@sortCategories')
 
 Route::post('/searchArticles', 'SearchController@searchArticles');
 
-Route::post('/logIn', 'UserController@logIn');
-Route::get('/logIn', 'UserController@logIn');
+
+
+Auth::routes();
+// Authentication Routes...
+/*$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+*/
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
