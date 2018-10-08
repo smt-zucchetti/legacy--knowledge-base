098a18066acc10610e4ce1b6ddbe6122b742108a
Steven Tune <Steven@Marks-MacBook-Pro-3.local>
2018-10-08 12:28:24 -0700

    Folder controller

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -23,18 +23,50 @@ class FolderController extends Controller
  		DB::table('Folders')->insert(array(
  			'Name' 			=> $_POST['name'], 
  			'dateCreated' 	=> date('Y-m-d H:i:s'),
+ 			'parentId'		=> !empty($_POST['parentId'])?$_POST['parentId']:null,
  			'createdBy'		=> Auth::user()->id
  		));
 
  		return self::readFolders();
  	}
 
+
+ 	public function __createFolderHierarchy($folders){ 		
+
+ 		//deep clone array of objects
+ 		$foldersCopy = array();
+	    foreach($folders as $key => $value) {
+	        $foldersCopy[$key] = clone $value;
+	    }
+
+	    //reorganize as needed into hierarchy
+ 		foreach($foldersCopy as $key => $folderA){
+ 			if(!property_exists($folderA, 'childFolders')){
+ 				$folderA->childFolders = array();
+ 			}
+ 			if($folderA->parentId !== null){
+ 				foreach($foldersCopy as $folderB){
+ 					if($folderA->parentId === $folderB->id){
+ 						$folderB->childFolders[] = $folderA;
+ 						unset($foldersCopy[$key]);
+ 					}
+ 				}
+ 			}
+ 		}
+
+ 		//print_r($foldersCopy);
+ 		//die();
+
+ 		return $foldersCopy;
+ 	}
+
  	public function readFolders(){
  		if (Auth::user()){
 
  			$folders = DB::table('Folders')->orderBy('dateCreated', 'DESC')->get();
+ 			$folderHierarchy = self::__createFolderHierarchy($folders);
 
- 			return view('readFolders', array('folders' => $folders, 'sorted' => array(false)) );
+ 			return view('readFolders', array('folders' => $folders, 'folderHierarchy' => $folderHierarchy, 'sorted' => array(false)) );
  		}else{
  			return view('home');
  		}
@@ -46,7 +78,8 @@ class FolderController extends Controller
 	 		->update(array(
 	 			'Name' 			=> $_POST['name'],
 	 			'lastUpdated' 	=> date('Y-m-d H:i:s'),
-	 			'lastUpdatedBy'	=> Auth::user()->id
+	 			'lastUpdatedBy'	=> Auth::user()->id,
+	 			'parentId'		=> !empty($_POST['parentId'])?$_POST['parentId']:null
  		));
 
  		return self::readFolders();
