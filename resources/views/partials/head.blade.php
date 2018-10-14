b0f0a881cebcd59de225f5b0c3986598fcb53e97
Steven Tune <Steven@Marks-MacBook-Pro-3.local>
2018-10-13 21:21:34 -0700

    added APP_URL env var as base href in head file (relative URLs for assets

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <base href="{{env('APP_URL')}}" />
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -14,6 +15,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
 
     <link rel="stylesheet" href="resources/css/main.css" />
     
@@ -34,4 +36,3 @@
    	<!--JQuery UI-->
     <link rel="stylesheet" href="resources/jquery-ui/jquery-ui.min.css">
     <script src="resources/jquery-ui/jquery-ui.min.js"></script>
-    <link rel="stylesheet" href="resources/css/my-jquery-ui.css">
