2afce4639c79de82c22bebf369b913eaf1a76fba
Steven Tune <Steven@Marks-MacBook-Pro-3.local>
2018-10-13 21:45:04 -0700

    added curated mini menu to home view (after successful login)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,15 @@
         @endif
 
         You are logged in!
+        <br><br>
+        <h3>Places to Visit</h3>
+        <ul class="loggedInLinks">
+        	<li><a href="createArticle">Create Article</a></li>
+        	<li><a href="articleList">Article List</a></li>
+        	<li><a href="">Dashboard</a></li>
+        	<li><a href="readFolders">Folder List</a></li>
+        	<li><a href="readCategories">Category List</a></li>
+        </ul>
     </div>
       
 @endsection
