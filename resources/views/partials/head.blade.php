2a68c019946fd5fa017be954020ad467fd21cec6
Steven Tune <Steven@Marks-MacBook-Pro-3.local>
2018-10-15 13:47:56 -0700

    filemanager asset path test

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <base href="{{env('APP_URL')}}/" />
+  
     
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
