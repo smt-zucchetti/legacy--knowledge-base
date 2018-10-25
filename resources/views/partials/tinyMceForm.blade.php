2b57f2535761ff585da400ed1f33986e949fe1c8
Steven Tune <Steven@Marks-MacBook-Pro-3.local>
2018-10-24 23:04:29 -0700

    removed uneeded blades

--- resources/views/partials/tinyMceForm.blade.php
+++
@@ -1,7 +0,0 @@
-
-    <label for="content"><h3>Content:</h3> </label>
-    {{ Form::textarea('content', !empty($article->Content)?$article->Content:"", array('class' => 'form-control my-editor', 'id' => 'tinyMCE')) }}
-
-    <!--<textarea id="tinyMCE" name="content" class="form-control my-editor">{{!empty($article)?$article->Content:''}}</textarea>-->
-    <textarea id="textOnlyContent" name="textOnlyContent" >{{!empty($article)?$article->textOnlyContent:''}}</textarea>
-
