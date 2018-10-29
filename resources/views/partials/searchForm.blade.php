d5d7f6ddaa92359feee78588b321a6c8e57ced41
Steven Tune <Steven@Marks-MacBook-Pro-3.local>
2018-10-29 12:13:16 -0700

    style updates/fixes

--- resources/views/partials/searchForm.blade.php
+++ resources/views/partials/searchForm.blade.php
@@ -1,6 +1,6 @@
 <form class="smallSearchForm" method="post" action="<?php echo url('/searchArticles') ?>" >
    @csrf
-  Search: <input type="text" name="search" value="<?php echo !empty($searchTerm)?$searchTerm:''; ?>" />
+  <input placeholder="Search for article by title or content" type="text" name="search" value="<?php echo !empty($searchTerm)?$searchTerm:''; ?>" />
   <input type="submit" value="Search" />
 </form>
 <!--<a class="advSearch" href="<?php echo url('/search') ?>">Advanced Search</a>-->
