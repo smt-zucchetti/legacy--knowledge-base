b988c4a76ee7e88290931f30025436bc8983427b
Steven Tune <Steven@Marks-MacBook-Pro-3.local>
2018-10-29 11:06:19 -0700

    breaking the action items out into a partial

--- resources/views/partials/actionItems.blade.php
+++ resources/views/partials/actionItems.blade.php
@@ -1,4 +1,4 @@
-<div class="actionItems">
+<div class="actionItems actionItemsHeader">
 	@if(in_array('back', $items))
 		<a id="goBack" href="javascript:window.history.back();" >
 			<i class="fas fa-reply"></i>
@@ -28,4 +28,39 @@
 			Edit
 		</a>
 	@endif
+
+	@if(in_array('addCategory', $items) && Auth::check())
+		<div style="display: none;" id="addCategory">
+			<form method="post" action="{{ url('createCategory') }}">
+				@csrf
+				<h2>Add New Category</h2>
+				<div class="validationError hidden">Category Name is empty</div>
+				<input type="text" name="name" />
+				<input type="submit" value="Create" />
+			</form>
+		</div>
+		<a class="addCategory" data-fancybox data-src="#addCategory" href="javascript:;">
+			<i class="fas fa-plus"></i> Add New 
+		</a>
+	@endif
+
+
+	@if(in_array('addFolder', $items) && Auth::check())
+		<div style="display: none;" id="addFolder">
+			<form method="post" action="<?php echo url('createFolder');?>">
+				@csrf
+				<h2>Add New Folder</h2>
+				@include('partials/foldersSelectBox', ['curFolder' => null])
+				<label for="name">Name
+					<input type="text" name="name" id="name" value="{{!empty($curFolder)?$curFolder->name:''}}" />
+				</label>
+				<input type="submit" value="Create" />
+			</form>
+		</div>
+		<a class="addFolder" data-fancybox data-src="#addFolder" href="javascript:;">
+			<i class="fas fa-plus"></i> Add New 
+		</a>
+	@endif
+
+
 </div>
